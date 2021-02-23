d38a6e5b4c6963c42a7f0f917810c14352c95d85
RenoNovoa <djnova25@gmail.com>
2021-02-22 19:30:51 -0500

    Repo SetUp

---
+++ Mid-TermPOS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mid_TermPOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
