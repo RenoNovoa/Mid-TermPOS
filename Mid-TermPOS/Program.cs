8cfd472bc57998fc9ec66298873f8b71cfc465f9
jasmine saad <jasmineysaad@gmail.com>
2021-02-25 18:48:10 -0500

    GetAllProducts commit

--- Mid-TermPOS/Program.cs
+++ Mid-TermPOS/Program.cs
@@ -6,7 +6,32 @@ namespace Mid_TermPOS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            public static List<Product> GetAllProducts()
+            {
+                List<Product> output = new List<Product>();
+
+                List<string> storeItems = TextFile.GetItems(filePath);
+
+                foreach (var merch in storeItems)
+                {
+                    if (string.IsNullOrWhiteSpace(merch))
+                    {
+                        continue;
+                    }
+                    string[] item = merch.Split(',');
+                    Product products = new Product();
+
+                    products.Name = item[0];
+                    products.Category = item[1];
+                    products.Description = item[2];
+                    decimal.TryParse(item[3], out decimal PriceOfItems);
+                    products.PriceOfItems = PriceOfItems;
+
+
+                    output.Add(products);
+                };
+                return output;
+            }
         }
     }
 }
